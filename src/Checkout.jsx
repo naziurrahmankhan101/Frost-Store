f6c0a769e64aa7862e1b8a185da6d43f596a9b8b
Naziur Rahman Khan (Aiman) <naziurrahmankhan101@gmail.com>
2024-06-04 04:05:47 +0600

    navigation complete.

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import NavBar from './NavBar';
 
 const Checkout = () => {
   return (
     <div>
+      <NavBar />
         <h1 className="text-4xl font-bold text-center">
             Heyyy Checkout here!!
         </h1>
--- src/Wishlist.jsx
+++ src/Wishlist.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import NavBar from './NavBar';
 
 const Wishlist = () => {
   return (
     <div>
+      <NavBar />
         <h1 className="text-4xl font-bold text-center">
             Heyyy I'm wishlist!!
         </h1>
