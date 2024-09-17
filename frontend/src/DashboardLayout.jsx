c27982c53c3da8ff25166f05c6b6ac42bb42a1fb
Naziur Rahman Khan (Aiman) <naziurrahmankhan101@gmail.com>
2024-09-18 00:19:10 +0600

    Upload book feature in admin panel by Marjana Salauddin

--- frontend/src/DashboardLayout.jsx
+++ frontend/src/DashboardLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import {Outlet} from 'react-router-dom'
-import {SideBar} from './SideBar'
+import {SideBar} from './Sidebar'
 
 const DashboardLayout = () => {
   return (
--- frontend/src/EditBooks.jsx
+++ frontend/src/EditBooks.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import SideBar from './SideBar'
+import SideBar from './Sidebar'
+
 
 const EditBooks = () => {
   return (
@@ -8,7 +9,7 @@ const EditBooks = () => {
         <SideBar/>
       </div>
       EditBook
-    </div>
+    </div> 
   )
 }
 
