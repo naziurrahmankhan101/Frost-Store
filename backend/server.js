eedeab04ff62b35f928f4c83b15139e861c95b27
Naziur Rahman Khan (Aiman) <naziurrahmankhan101@gmail.com>
2024-09-26 21:15:39 +0600

    Auth backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 //mongo pass : JG5SC5xjAsR9MH4g username: naziurrahmankhan101
 
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -13,8 +15,10 @@ const app= express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, ()=> {
     console.log("Server is running on http://localhost:"+ PORT);
