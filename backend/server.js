876a5dc25f3927b047e0145a2a3ec189088929c3
Naziur Rahman Khan (Aiman) <naziurrahmankhan101@gmail.com>
2024-09-28 11:29:10 +0600

    finalizing cart page

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
-//mongo pass : JG5SC5xjAsR9MH4g username: naziurrahmankhan101
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
@@ -15,10 +14,12 @@ import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
-const app= express();
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -28,8 +29,15 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-app.listen(PORT, ()=> {
-    console.log("Server is running on http://localhost:"+ PORT);
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-    connectDB();
-})
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+	console.log("Server is running on http://localhost:" + PORT);
+	connectDB();
+});
